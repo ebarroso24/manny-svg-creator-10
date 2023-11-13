6980c412a756254d69244b9322001e4175ce28fc
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-12 22:18:39 -0500

    added to README

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # manny-svg-creator-10
+# <"Welcome to Manny's SVG Logo Creator!">
+
+## Description
+
+- What was your motivation? The purpose of this project was to learn about Object-oriented programming, and use the knowledge and research to create an SVG logo maker.
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+
+## Usage
+
+This SVG logo maker will prompt the user to choose 1 to 3 letters to go into the shape, choose a TEXT COLOR, SHAPE COLOR, and pick from 3 shapes: Circle, Square or Triangle. Once all selections are made, a link to the user's created SVG logo will appear in the logo.svg file. The user will need to open in live terminal to view their created shape. 
+
+This app is supposed to also use Jest to test the functionality of the app. This function is currently unavailable in this edition of the app.
+
+## Credits
+
+YouTube - Zackary B: https://www.youtube.com/watch?v=zh9CsKqEhOM
+Youtube: THOSCALLE: https://www.youtube.com/watch?v=GJYMcLus3v0
+NPM Jest: https://www.npmjs.com/package/jest?activeTab=readme
+MDN Web Docs: https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes#circle
+Chatgpt.com: https://chat.openai.com/
+
+## License
+MIT License
+
+Copyright (c) 2023 ebarroso24
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const filesystem = require('./node_modules/graceful-fs/graceful-fs')
 const inquirer = require("inquirer");
 const {Circle, Square, Triangle} = require("./lib/shapes");
-// Imports the graceful-fs, inquirer, Circle, Square, and Triangle modules.
-// Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
-
+ 
 class Svg{
     constructor(){
         this.textElement = ''
@@ -22,9 +20,6 @@ class Svg{
     }
     
 }
-
-// Defines array of 'questions' using the 'inquirer' library with the following questions.
-// Each question is an object that specifies the properties of TEXT, TEXT COLOR, SHAPE COLOR, and Pixel Image.
 const questions = [
     {
         type: "input",
@@ -48,8 +43,7 @@ const questions = [
         choices: ["Circle", "Square", "Triangle"],
     },
 ];
-
-// Function to write data to file
+ 
 function writeToFile(fileName, data) {
 	console.log("Writing [" + data + "] to file [" + fileName + "]")
     filesystem.writeFile(fileName, data, function (err) {
@@ -65,31 +59,31 @@ async function init() {
 	var svgString = "";
 	var svg_file = "logo.svg";
 
-    // Prompt the user for answers
+     
     const answers = await inquirer.prompt(questions);
 
-	//user text
+	 
 	var user_text = "";
 	if (answers.text.length > 0 && answers.text.length < 4) {
-		// 1-3 chars, valid entry
+		 
 		user_text = answers.text;
 	} else {
-		// 0 or 4+ chars, invalid entry
+		 
 		console.log("Invalid user text field detected! Please enter 1-3 Characters, no more and no less");
         return;
 	}
 	console.log("User text: [" + user_text + "]");
-	//user font color
+	 
 	user_font_color = answers["text-color"];
 	console.log("User font color: [" + user_font_color + "]");
-	//user shape color
+	 
 	user_shape_color = answers.shape;
 	console.log("User shape color: [" + user_shape_color + "]");
-	//user shape type
+	 
 	user_shape_type = answers["pixel-image"];
 	console.log("User entered shape = [" + user_shape_type + "]");
 	
-	//user shape
+	 
 	let user_shape;
 	if (user_shape_type === "Square" || user_shape_type === "square") {
 		user_shape = new Square();
@@ -108,15 +102,13 @@ async function init() {
 	}
 	user_shape.setColor(user_shape_color);
 
-	// Create a new Svg instance and add the shape and text elements to it
+	 
 	var svg = new Svg();
 	svg.setTextElement(user_text, user_font_color);
 	svg.setShapeElement(user_shape);
 	svgString = svg.render();
 	
-	//Print shape to log
 	console.log("Displaying shape:\n\n" + svgString);
-	//document.getElementById("svg_image").innerHTML = svgString;
 
 	console.log("Shape generation complete!");
 	console.log("Writing shape to file...");
